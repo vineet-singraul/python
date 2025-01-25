538b6e06d17667fb29c6799be8ad3af9743870c1
Vineet Singraul <vineetsingh5838@gmail.com>
2025-01-25 19:20:05 +0530

    new commit

---
+++ _17__Slicing/ex3.py
@@ -0,0 +1,6 @@
+my_list = [10,20,30,40,50,60,70]
+print(my_list[::2])  # Output: [0, 2, 4, 6, 8] (every 2nd element)
+
+print(my_list[::-1])  # Output: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+print(my_list[1:8:3])  # Output: [1, 4, 7]
