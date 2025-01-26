5faefa25fcecbfcdb27dcebd3e9785adf48d92ba
Vineet Singraul <vineetsingh5838@gmail.com>
2025-01-26 21:20:34 +0530

    new commit

--- _17__Slicing/ex8.py
+++ _17__Slicing/ex8.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-my_list = [10, 20, 30, 40, 50]
-print(my_list[1:3])
-=======
+
 text = input("Enter a sentence: ")
 
 first_half = text[:len(text)//2]
 
 print("First half of the sentence:", first_half)
->>>>>>> 06541e6f39fc6bd5816d617ec37d9bdf59b1ee64
+
