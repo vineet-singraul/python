0ce0f3d7af4032b432516c38844ef9105edb43c4
Vineet Singraul <vineetsingh5838@gmail.com>
2025-01-30 19:03:16 +0530

    new commit

---
+++ _20---List_method/create_method/appent_method.py
@@ -0,0 +1,7 @@
+l = [10,20,"Vineet",(10,20,30),{30,40}]
+l.append("Vineet")
+print(l)
+
+
+l.append([22,3,4,56,87,434])
+print(l)
---
+++ _20---List_method/create_method/extent_method.py
@@ -0,0 +1,3 @@
+li = [10, 20, 'Vineet', (10, 20, 30), {40, 30}, 'Vineet']
+li.extend([33,445,556])
+print(li)
