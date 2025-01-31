672d3f7cc9d9f1b4d87875060d4d0cb4dedf7f55
Vineet Singraul <vineetsingh5838@gmail.com>
2025-01-31 18:53:18 +0530

    new code

---
+++ _21---Tuple_method/max().py
@@ -0,0 +1,6 @@
+
+li = (10,20,30,40,50,60)
+name = ("vineet" ,"Disha","Vineet")
+
+print(max(li))
+print(max(name))
---
+++ _21---Tuple_method/sum().py
@@ -0,0 +1,6 @@
+
+li = (10,20,30,40,50,60)
+# name = ("vineet" ,"Disha","Vineet")
+
+print(sum(li))
+# print(sum(name))
