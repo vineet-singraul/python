14897461f8b04603ab39a82829be2b01adcd62dc
Vineet Singraul <vineetsingh5838@gmail.com>
2025-01-31 17:22:39 +0530

    new code

---
+++ _00---Practice--Slicing/examples.py
@@ -0,0 +1,20 @@
+#  -14 -13 -12 -11 -10  -9  -8  -7  -6  -5  -4  -3  -2  -1   # Negative Indexing
+#   v   i   n   e   e   t   s   i   n   g   r   a   u   l
+#      0   1   2   3   4   5   6   7   8   9  10  11  12  13   # Positive Indexing
+
+s = "vineetSingraul"
+
+# print(s[2:6:])  #neet
+# p = print(s[2:6:-1])  
+# print(p)   #None
+
+# p = print(s[-2:6:-1])   #uargni
+
+
+# p = print(s[-2:-6:1])  
+
+# p = print(s[-2:-1:-1])
+p = print(s[-4:-2:-1])
+
+
+# a = print(s[::])
---
+++ _00---String--Practice/concatination.py
@@ -0,0 +1,17 @@
+# a = "Vineet"
+# b = "Singh"
+# c = "2345"
+# d = "@gmail.com"
+# e = a+b+c+d
+# print(e)
+
+# VineetSingh2345@gmail.com
+
+
+# ------------------------------------------
+
+
+
+x = 10
+y = "vineet"
+print(x+y)
