746a3b2be1a4efb8245e712c6626a3903d9093f4
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-09 13:50:26 +0530

    new code

---
+++ _21---Tuple_method/Tuple__Practice__set/examples__function__with.py
@@ -0,0 +1,20 @@
+tu = (2,4,5,1,4,6,8,6,3)
+
+print("Max Value Is : ",max(tu))
+# Max Value Is :  8
+
+
+print("Min Value Is : ",min(tu))
+# Min Value Is :  1
+
+
+print("length Value Is : ",len(tu))
+# len Value Is :  9
+
+
+print("Data type Value Is : ",type(tu))
+# Dtaa type Value Is :  <class 'tuple'>
+
+
+print("Data id Value Is : ",id(tu))
+# Data id Value Is :  1778812358096
---
+++ _21---Tuple_method/Tuple__Practice__set/exampls1.py
@@ -0,0 +1,10 @@
+# Printing Techniqes Of Python
+tu = (10,20,30,40,50,60)
+print(tu)      #(10, 20, 30, 40, 50, 60)
+print(tu[:])   #(10, 20, 30, 40, 50, 60)
+
+# slicing in python 
+tu2 = (2,4,5,1,4,6,8,6,3)
+print(tu2[-3:1:-1])
+print(tu2[3:-1:-1])
+print(tu2[3:1:-1])
