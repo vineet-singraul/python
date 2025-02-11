b3903594f846827846a5dadb469777f9fc37ad80
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-11 12:52:04 +0530

    new code

---
+++ _22___dicsionary--method/Basic_Dict.py
@@ -0,0 +1,21 @@
+# dics = {1:"Vineet" , 2:"Mohni", 3 : "Anmol"}
+# dics1 = {1:"Vineet" , 2:"Mohni", 3 : "Anmol"}
+# print(dics)
+
+# print(id(dics))
+# print(id(dics1))
+
+# dics.update(2,"Raja")
+# print(dics)
+
+d1 = {1:"Vineet" , 2:"Mohni", 3 : "Anmol"}
+
+print("The Max Value Is = ",max(d1),max(d1.values()))
+
+print("The Min Value Is = ",min(d1),min(d1.values()))
+
+print("The Type Value Is = ",type(d1))
+
+print("The Lenght Value Is = ",len(d1))
+
+
