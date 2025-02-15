4bfe2f21df805e5a0f94bcc9d131afe03a641dee
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-15 19:05:03 +0530

    new code

---
+++ _33___Functions_Of_pythone/Defualt Argument/example1.py
@@ -0,0 +1,14 @@
+def add (x=0,y=0,z=0):
+    print("x",x)
+    print("y",y)
+    print("z",z)
+    return x+y+z
+
+p = 10
+q = 10
+r = 10
+# res = add(p,q ,r)
+res = add()
+print(res)
+
+# Defalt-Value Argument  argument me pahle se set ker diya yaha 
---
+++ _33___Functions_Of_pythone/Postional/example1.py
@@ -0,0 +1,13 @@
+def add (x,y,z):
+    print("x",x)
+    print("y",y)
+    print("z",z)
+    return x+y+z
+
+p = 10
+q = 10
+r = 10
+res = add(z=p,y=q ,x=r)
+print(res)
+
+# Defalt-Value Argument  argument me pahle se set ker diya yaha 
