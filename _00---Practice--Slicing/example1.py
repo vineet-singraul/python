0d1301382382af6f610d4cf3c6817cfd6236a162
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-16 21:08:42 +0530

    new code

---
+++ _00---Practice--Slicing/example1.py
@@ -0,0 +1,17 @@
+numbers = [10, 20, 30, 40, 50, 60, 70]
+
+
+print(numbers[1:5])  
+ # Output: [20, 30, 40, 50]
+
+
+print(numbers[:4])    
+# Output: [10, 20, 30, 40]
+
+
+print(numbers[3:])   
+ # Output: [40, 50, 60, 70]
+
+
+print(numbers[::2])  
+ # Output: [10, 30, 50, 70] 
--- _33___Functions_Of_pythone/Variable lenght Positional Argu/ex1.py
+++ _33___Functions_Of_pythone/Variable lenght Positional Argu/ex1.py
@@ -1,3 +1,16 @@
+# def add (*n):
+#     print(n)
+#     print(type(n))
+#     sum = 0
+#     for i in n:
+#         sum = sum + i
+#     return sum
+# res = add(2,3,4,5,6)
+# print(res)
+
+
+# dynamic input
+
 def add (*n):
     print(n)
     print(type(n))
@@ -5,5 +18,7 @@ def add (*n):
     for i in n:
         sum = sum + i
     return sum
-res = add(2,3,4,5,6)
+
+x = eval(input("Enter One Number:"))
+res = add(x)
 print(res)
