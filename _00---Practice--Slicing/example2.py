b94c5d79b1151fa17dabed45bb1ffd84b72f645b
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-16 21:10:18 +0530

    new code

---
+++ _00---Practice--Slicing/example2.py
@@ -0,0 +1,17 @@
+text = "Hello, World!"
+
+
+print(text[0:5])   
+ # Output: 'Hello'
+
+
+print(text[:5])     
+# Output: 'Hello'
+
+
+print(text[7:])    
+ # Output: 'World!'
+
+
+print(text[::-1])  
+ # Output: '!dlroW ,olleH' 
