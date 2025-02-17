56eda8cbf073058cf576bb535c7d8b176e6868dd
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-17 19:18:30 +0530

    new code

--- _33___Functions_Of_pythone/Keyword_variable__lengh_argu/ex.py
+++ _33___Functions_Of_pythone/Keyword_variable__lengh_argu/ex.py
@@ -14,17 +14,24 @@
 # def Add(**kwards):
 #     print(kwards)
 #     print(type(kwards))
-
 # res = Add(z=10 , e=20 , f=30 , t =10)
-
 # print(res)
 
 
 
+def Add(**kwards):
+    for i in kwards.items():
+     print(i)
+diction = eval(input("Enter A Dictionary : "))
+res = Add(**diction)
+
+
 
 def Add(**kwards):
-    print(kwards)
-    print(type(kwards))
+
+    for k,v in kwards.items():
+      print(f'My {k} is : {v}')
+      print(type(kwards))
 
 
 diction = eval(input("Enter A Dictionary : "))
