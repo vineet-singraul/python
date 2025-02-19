86ecdf8759a4380ca4e1175b4dfe5cc62e503b0d
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-19 19:21:05 +0530

    new code

---
+++ _34___Loca_and_globle/ex1.py
@@ -0,0 +1,23 @@
+# x=10
+# print(x)
+
+# def Add():
+#     y=20
+#     print(x)
+
+# Add()
+# print(x)
+# # print(y)  NameError: name 'y' is not defined
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ _34___Loca_and_globle/ex2.py
@@ -0,0 +1,26 @@
+# x = 20
+# def Update(p):
+#     global x
+#     x = p
+#     print(x)
+
+# print("Before Update Call:", x) 
+# y = int(input("Enter Any Call: ")) 
+# Update(y)  
+# print("After Update Call:", y)
+
+
+
+
+# for make the globle keys to local
+x = 10
+def Update(p):
+    global x
+    print("Before Update Call:", globals()['x']) 
+    x = p
+    print(x)
+
+y = int(input("Enter Any Call: ")) 
+print("Befor Updated : ",x)
+Update(y)  
+print("After Update Call:", x)
