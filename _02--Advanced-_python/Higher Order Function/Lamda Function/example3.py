e6e159555e2978bc82b1c4adc91838bbbc6d432c
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-25 19:26:31 +0530

    new code

--- _02--Advanced-_python/Higher Order Function/Lamda Function/example3.py
+++ _02--Advanced-_python/Higher Order Function/Lamda Function/example3.py
@@ -2,9 +2,29 @@
 
 # res = lambda li : 
 
-li = [1,2,3]
-li2 = []
+# li = [1,2,3]
+# li2 = []
 
-for i in li:
-    li2.append(li)
-print(li2)
+# for i in li:
+#     li2.append(li)
+# print(li2)
+
+
+
+
+#  solve using thise lemda
+
+# syntax 
+#  lambda x : [ i for i in colloction]
+
+x = "Vineet"
+z = lambda x : [p for p in x]
+print(z(x))
+
+
+# or
+
+
+y = [1,2,3,4,5,6]
+z = lambda y : [ i  for i in y]
+print(z(y))
