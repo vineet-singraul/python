fd1abcab944178ba90604c1d32f3813a66d7e125
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-27 19:17:09 +0530

    new code

--- _02--Advanced-_python/Genrator/example.py
+++ _02--Advanced-_python/Genrator/example.py
@@ -7,6 +7,10 @@
 
 
 
+
+# genratoe -----> key word hai
+
+
 # def first():
 #     yield "hello"
 
@@ -15,19 +19,48 @@
 
 
 
-def first():
-    yield 1
-    yield 2
-    yield 3
+# def first():
+#     yield 10
+#     yield 2
+#     yield 3
 
-x = first()
-print(x)
-print(next(x))
-print("hi")
-print("hiiiii")
-print("hello")
-print(next(x))
+# x = first()
+# print(x)
+# print(next(x))
+# print("hi")
+# print("hiiiii")
+# print("hello")
+# print(next(x))
 
-print(next(x))
+# print(next(x))
 
 # print(list(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def natralNum(x):
+    i=1
+    while i<=x:
+        yield i
+        i = i + 1
+
+n = 10
+p = natralNum(10)
+
+print(next(p))
+for i in p:
+    print(i)
+    print("vineet")
