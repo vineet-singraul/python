c1b6d7cdfac498d0ec4d77960f4647662cd6f057
Vineet Singraul <singraulvineet943@gmail.com>
2025-03-05 18:52:34 +0530

    new commit

--- _02--Advanced-_python/OOP'S/Folder1/example.py
+++ _02--Advanced-_python/OOP'S/Folder1/example.py
@@ -1,6 +1,10 @@
 class Student:
-    def __init__(self):
+    def __init__():
         print("Constructor clled :")
     
 obj = Student
 # obj = Student()
+
+
+# constructor ko baher se called ker sakte hai
+obj.__init__()
