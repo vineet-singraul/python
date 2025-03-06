1eb486c34e875800b93b1c6be017bdda373a5c41
Vineet Singraul <singraulvineet943@gmail.com>
2025-03-06 19:22:08 +0530

    new code

---
+++ _02--Advanced-_python/OOP'S/Variables/Local_Variable.py
@@ -0,0 +1,11 @@
+# Aisa variable jo block ke aander keval acess ho LOCAL Var;
+# instance - and  -  Static  variable ----> Kabhi bhi local variable nhi ho sakta hai
+
+class Student:
+
+    def __init__():
+        p = 10
+        print(p)
+
+
+obj1 = Student()
--- _02--Advanced-_python/OOP'S/Variables/Static Variables.py
+++ _02--Advanced-_python/OOP'S/Variables/Static Variables.py
@@ -1,1 +1,33 @@
 # Aise var vo object per depend nhi karte {aleg aleg object me bhi same  value dega}
+
+# jaise koi value change nhi karna hau=i to to usko static varable me define ker de
+
+
+# class Student:
+#     school = "SSSH"
+#     city = "Bhopal"
+
+#     def __init__(self, name , roll):
+#         self.x = name
+#         self.y = roll
+
+#     def show(self):
+#         print(self.x , self.y)
+#         print("Student School : ",Student.school)
+#         print("Student City  : ",Student.city)
+
+
+# obj1 = Student("Vineet", 111)
+# obj1.show()
+
+
+
+
+# -----------------------------------------------------------
+
+
+
+
+# Declear Kaha ker sakte hai
+
+111
