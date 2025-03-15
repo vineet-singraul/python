f1db4d9e5dbc934ec49e1a514f539cb6d638d793
Vineet Singraul <singraulvineet943@gmail.com>
2025-03-15 09:40:24 +0530

    new code

--- _01--Core__Python/_12___Tuple_in_python/ex.py
+++ _01--Core__Python/_12___Tuple_in_python/ex.py
@@ -1,10 +1,20 @@
-def medium_range_tuple(t):
-    if not t:
-        return None 
-    min_val = min(t)
-    max_val = max(t)
+# def medium_range_tuple(t):
+#     if not t:
+#         return None 
+#     min_val = min(t)
+#     max_val = max(t)
     
-    return (min_val + max_val) / 2
-t = (10, 20, 30, 40, 50)
-result = medium_range_tuple(t)
-print("Medium Range:", result)
+#     return (min_val + max_val) / 2
+# t = (10, 20, 30, 40, 50)
+# result = medium_range_tuple(t)
+# print("Medium Range:", result)
+
+
+
+
+
+
+
+t16 = (1, 2, 2, 3, 3, 3, 4)
+print(t16.count(3)) 
+print(t16.index(4))
