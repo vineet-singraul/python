375f1668aa24bf6760400800c94047ab0260aee7
Vineet Singraul <singraulvineet943@gmail.com>
2025-05-19 04:40:56 +0530

    new code

---
+++ PY_04--Home_in_Practice/Data-type/Rules_of_python_with_prof/prac_int_data_type.py
@@ -0,0 +1,27 @@
+# import sys
+
+# a = 10
+# print("Size of value a:", sys.getsizeof(a))
+
+
+# b = 5000002277
+# print("Size of value b:", sys.getsizeof(b))
+
+
+# a = 10
+# print(bin(a))
+# print(hex(a))
+# print(oct(a))
+
+
+a = 10
+b = bin(a)
+print(b)
+c = hex(b)
+print(c)
+d = int(c)
+print(d)
+# d = oct(c)
+# print(d)
+# f = int(d)
+# print(f)
