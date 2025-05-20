0fe07f6f4257ea2107043327131a943af98b7c8d
Vineet Singraul <singraulvineet943@gmail.com>
2025-05-20 14:19:01 +0530

    new code

---
+++ PY_04--Home_in_Practice/Operator-Slice/ex.py
@@ -0,0 +1,9 @@
+# s = "Computer"
+# print(s[6:1:-1])
+
+
+s = "Ramayana"
+# print(s[6:-1:1])
+# print(s[-1:3:-1])
+print(s[-1:-3:-1])
+print(s[-3:-1:-1])
---
+++ PY_04--Home_in_Practice/Operator-Slice/explicit_tC.py
@@ -0,0 +1,13 @@
+x = 5
+print("Convert into float : ",float(x))
+print("Convert into String : ",str(x))
+print("Convert into complex : ",complex(x))
+print("Convert into Boolean : ",bool(x))\
+
+
+
+print("------------------------------")
+
+y = '5'
+z = int(y)
+print("Convert into integer : ",type(z))
